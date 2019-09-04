5fe992379975207b8722933480e8679e307e9940
leonardogd <leonardogd@localhost>
2019-09-04 15:35:50 -0300

    Final_Funcionando Alteração para java 8

--- CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/Main.java
+++ CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/Main.java
@@ -1,30 +1,56 @@
 package br.lgomesd.cifradecesar;
 
-import java.util.Locale;
+import java.io.IOException;
 import java.util.Scanner;
 
-
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
 public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char alfabetoChar[] = alfabeto.toCharArray();
+                
+        Scanner ler = new Scanner(System.in);
+                
+        int N = ler.nextInt();
+        
+        for(int a=1;a<=N;a++){
+            
+                   
+            String sentencaCodString = ler.next();
+            char sentencaDecod[] = new char[sentencaCodString.length()];
+            
+            int nCifra = ler.nextInt();
+            
+            for(int i=0;i<sentencaCodString.length();i++)
+                for(int x=0;x<alfabetoChar.length;x++){
+                    if(x-nCifra >= 0){
+                        if(alfabetoChar[x] == sentencaCodString.charAt(i)){
+                            sentencaDecod[i] = alfabetoChar[x-nCifra];
+                            break;
+                        }
+                    }
+                    if(x-nCifra < 0){
+                        if(alfabetoChar[x] == sentencaCodString.charAt(i)){
+                            int b = (x-nCifra)*-1;
+                            int y = 26-b;
+                            sentencaDecod[i] = alfabetoChar[y];
+                            break;
+                        }
+                    }
+                }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.useLocale(Locale.ENGLISH);
-		Locale.setDefault(new Locale("en", "US"));
-		
-		int N = sc.nextInt();
-		for (int i=0 ; i<N ; i++){
-			sc.nextLine();
-			String str = sc.nextLine();
-			int troca=sc.nextInt();
-			char[] s = str.toCharArray();
-			for (int x=0 ; x<s.length ; x++){
-				if((s[x]-troca) < 65)
-					s[x]+=(26-troca);
-				else
-					s[x]-=troca;
-			}
-			System.out.println(s);
-		}
-		sc.close();
-	}
+            System.out.println(sentencaDecod);
+
+        }
+ 
+    }
+ 
 }
