6dfd4c0d4960ddf354d3a8cdf0119a384845b435
Leonardo Gomes Duarte <lgome@LAPTOP-LRE3EK8M>
2019-09-03 22:26:54 -0300

    Merge origin/master
    Conflicts:
    	CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/CifraDeCesar.java

--- CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/CifraDeCesar.java
+++ CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/CifraDeCesar.java
@@ -5,6 +5,8 @@
  */
 package br.lgomesd.cifradecesar;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lgome
@@ -16,8 +18,37 @@ public class CifraDeCesar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Teste Git");
-        System.out.println("Teste local");
+        //String alfabeto[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char alfabetoChar[] = alfabeto.toCharArray();
+        int nCasos = 0;
+        char sentencaDecod[] = new char[50];
+        String sentencaCodString = new String();
+        String sentencaCodArray[] = new String[50];
+        int nCifra;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Entre com o número de casos de teste");
+        nCasos = ler.nextInt();
+        
+        System.out.println("Entre com a cifra");
+        nCifra = ler.nextInt();
+        
+        Scanner lerStr = new Scanner(System.in);
+        System.out.println("Entre com a mensagem codificada");
+        sentencaCodString = lerStr.nextLine();
+        
+        for(int i=0;i<=sentencaCodString.length();i++)
+            for(int x=0;x<=alfabetoChar.length;x++)
+                if(alfabetoChar[x] == sentencaCodString.charAt(i)){
+                    sentencaDecod[i] = alfabetoChar[x];
+                    break;
+                }
+                    
+        System.out.println(sentencaDecod);
+          
+        //for(int i=0;i<sentencaV.length;i++)
+         //   System.out.print(sentencaV[i]);
     }
     
 }
